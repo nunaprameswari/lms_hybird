c99ecd113c3a9646dc01d35acdb9d54c94bd2534
nunaparmeswari <nuna.prameswari0930@gmail.com>
2025-12-21 18:22:09 -0800

    pembuatan splash & tambah file login.dart

---
+++ lib/Login/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginLms extends StatefulWidget {
+  const LoginLms({super.key});
+
+  @override
+  State<LoginLms> createState() => _LoginLmsState();
+}
+
+class _LoginLmsState extends State<LoginLms> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
