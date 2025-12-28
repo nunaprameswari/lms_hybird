85b0fd331d287bda63005e659394c463eddc6c22
nunaparmeswari <nuna.prameswari0930@gmail.com>
2025-12-28 10:01:24 -0800

    melakukan editing sebuah home

--- lib/fitur/home.dart
+++ lib/fitur/home.dart
@@ -32,84 +32,54 @@ class Beranda extends StatelessWidget {
 
       body: SafeArea(
         child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
           padding: const EdgeInsets.all(16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // ===== Header =====
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text('Hallo,', style: TextStyle(color: Colors.grey)),
-                      Text(
-                        'DANDY CANDRA PRAITAMA',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const Profile(),
-                        ),
-                      );
-                    },
-                    child: Chip(
-                      backgroundColor: primaryColor,
-                      label: const Text(
-                        'MAHASISWA',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      avatar: const Icon(Icons.person, color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-
-              // ===== Tugas =====
-              const Text(
-                'Tugas Yang Akan Datang',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-
+              // ===== HEADER =====
               Container(
                 width: double.infinity,
                 padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: const Color(0xFF0B0547),
-                  borderRadius: BorderRadius.circular(20),
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 103, 71, 248), // biru play
+                  // borderRadius: BorderRadius.vertical(
+                  //   bottom: Radius.circular(20),
+                  // ),
                 ),
-                child: Column(
-                  children: const [
-                    Text(
-                      'DESAIN ANTARMUKA & PENGALAMAN PENGGUNA',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Hallo,', style: TextStyle(color: Colors.grey)),
+                        Text(
+                          'DANDY CANDRA PRAITAMA',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Profile(),
+                          ),
+                        );
+                      },
+                      child: Chip(
+                        backgroundColor: Color(0xFF0B0547),
+                        label: const Text(
+                          'MAHASISWA',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        avatar: const Icon(Icons.person, color: Colors.white),
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Tugas 01 – UID Android Mobile Game',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    Text(
-                      'Waktu Pengumpulan\nJumat 26 Februari, 23:59 WIB',
-                      style: TextStyle(color: Colors.white),
-                      textAlign: TextAlign.center,
                     ),
                   ],
                 ),
@@ -117,7 +87,18 @@ class Beranda extends StatelessWidget {
 
               const SizedBox(height: 20),
 
-              // ===== Pengumuman =====
+              // ===== TUGAS =====
+              const Text(
+                'Tugas Yang Akan Datang',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+
+              _tugasCard(),
+
+              const SizedBox(height: 20),
+
+              // ===== PENGUMUMAN =====
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: const [
@@ -141,7 +122,7 @@ class Beranda extends StatelessWidget {
 
               const SizedBox(height: 20),
 
-              // ===== Progres Kelas =====
+              // ===== PROGRES KELAS =====
               const Text(
                 'Progres Kelas',
                 style: TextStyle(fontWeight: FontWeight.bold),
@@ -149,13 +130,16 @@ class Beranda extends StatelessWidget {
 
               const SizedBox(height: 10),
 
-              _progressItem(
-                'DESAIN ANTARMUKA & PENGALAMAN PENGGUNA',
-                '89% Selesai',
-                0.89,
-              ),
-              _progressItem('KEWARGANEGARAAN', '86% Selesai', 0.86),
-              _progressItem('SISTEM OPERASI', '90% Selesai', 0.90),
+              _progressItem('DESAIN ANTARMUKA & PENGALAMAN PENGGUNA', 0.89),
+              _progressItem('KEWARGANEGARAAN', 0.86),
+              _progressItem('SISTEM OPERASI', 0.90),
+              _progressItem('PEMROGRAMAN MULTIMEDIA INTERAKTIF', 0.90),
+              _progressItem('BAHASA INGGRIS: BUSINESS & SCIENTIFIC', 0.90),
+              _progressItem('OLAH RAGA', 0.90),
+
+              const SizedBox(
+                height: 80,
+              ), // ruang agar tidak tertutup bottom nav
             ],
           ),
         ),
@@ -163,9 +147,40 @@ class Beranda extends StatelessWidget {
     );
   }
 
-  Widget _progressItem(String title, String subtitle, double value) {
+  Widget _tugasCard() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color(0xFF0B0547),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: const [
+          Text(
+            'DESAIN ANTARMUKA & PENGALAMAN PENGGUNA',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Tugas 01 – UID Android Mobile Game',
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Waktu Pengumpulan\nJumat 26 Februari, 23:59 WIB',
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _progressItem(String title, double value) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(vertical: 10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -178,7 +193,10 @@ class Beranda extends StatelessWidget {
             minHeight: 6,
           ),
           const SizedBox(height: 4),
-          Text(subtitle, style: const TextStyle(fontSize: 11)),
+          Text(
+            '${(value * 100).toInt()}% Selesai',
+            style: const TextStyle(fontSize: 11),
+          ),
         ],
       ),
     );
